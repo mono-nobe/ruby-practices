d8f727c6742bbf2d4e799da2c5c6d6b2e9c76f34
mononobe <ryoya.goemono@gmail.com>
2022-02-05 14:50:28 +0900

    動作確認できたところまで完了

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,3 +1,46 @@
 #!/usr/bin/env ruby
 
 # frozen_string_literal: true
+
+score = []
+args = ARGV.shift.split(',')
+p args
+args.each do |arg|
+  if arg == 'X'
+    score.push(10)
+    score.push(0) if score.size < 18
+  else
+    score.push(arg.to_i)
+  end
+end
+
+p score
+sum = 0
+flame = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+flame.each do |f|
+  first_score = score.shift
+  second_score = score.shift
+
+  if first_score == 10
+    if score[0] == 10 && f != 9
+      sum += first_score + score[0] + score[2]
+      next
+    end
+
+    sum += first_score + score[0] + score[1]
+    next
+  end
+
+  if first_score + second_score == 10
+    sum += first_score + second_score + score[0]
+    next
+  end
+
+  sum += first_score + second_score
+end
+
+p score
+sum += score.sum
+
+p sum
