1d530ffc5be460e8a661f693a27c551cb106c4cc
mononobe <ryoya.goemono@gmail.com>
2022-02-20 10:23:15 +0900

    レビュー修正
    不要なコメントの削除
    spare_pointメソッドの追加
    マジックナンバーの廃止
    タイポ修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,20 +2,18 @@
 
 # frozen_string_literal: true
 
-# 1フレーム毎の点数を返す
-def points_for_flame(first_score, second_score)
+POINT_SIZE_FOR_NINE_FRAME = 18
+
+def point_for_frame(first_score, second_score)
   first_score + second_score
 end
 
-# 投げた結果を数値にして返す
 def throw_points(scores)
   points = []
   scores.each do |score|
-    # ストライクの場合
     if score == 'X'
       points.push(10)
-      # 9フレーム目まではストライクの場合[10, 0]とする
-      points.push(0) if points.size < 18
+      points.push(0) if points.size < POINT_SIZE_FOR_NINE_FRAME
     else
       points.push(score.to_i)
     end
@@ -24,42 +22,40 @@ def throw_points(scores)
   points
 end
 
-# ストライクの場合のポイント
-def strike_point(points, flame)
-  # 次の投球結果がストライクかつ9フレームではない場合
-  if points[0] == 10 && flame != 9
+def strike_point(points, frame)
+  if points[0] == 10 && frame != 9
     10 + points[0] + points[2]
   else
     10 + points[0] + points[1]
   end
 end
 
-throw_resutls = ARGV.shift.split(',')
-points = throw_points(throw_resutls)
+def spare_point(frame_point, next_point)
+  frame_point + next_point
+end
+
+throw_results = ARGV.shift.split(',')
+points = throw_points(throw_results)
 
 sum_score = 0
-# 9フレームまで同じ処理を繰り返す
-flames = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+frames = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-flames.each do |flame|
+frames.each do |frame|
   first_point = points.shift
   second_point = points.shift
 
-  # ストライクの場合
   if first_point == 10
-    sum_score += strike_point(points, flame)
+    sum_score += strike_point(points, frame)
     next
   end
 
-  # スペアの場合
-  if points_for_flame(first_point, second_point) == 10
-    sum_score += points_for_flame(first_point, second_point) + points[0]
+  if point_for_frame(first_point, second_point) == 10
+    sum_score += spare_point(point_for_frame(first_point, second_point), points[0])
     next
   end
 
-  sum_score += points_for_flame(first_point, second_point)
+  sum_score += point_for_frame(first_point, second_point)
 end
 
-# 10フレームの投球ポイントを加算する
 sum_score += points.sum
 p sum_score
