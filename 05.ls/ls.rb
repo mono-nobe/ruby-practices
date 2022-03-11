5ccee6de8002258415557fb66618d16f2ab8e96c
mononobe <ryoya.goemono@gmail.com>
2022-03-12 08:19:25 +0900

    個数を示す単語の修正
    - num_of_A => A_count

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,10 +2,10 @@
 
 # frozen_string_literal: true
 
-NUM_OF_COLUMNS = 3
+COLUMN_COUNT = 3
 
-def calc_num_of_row(num_of_dir_items)
-  (num_of_dir_items / NUM_OF_COLUMNS.to_f).ceil
+def calc_row_count(dir_items_count)
+  (dir_items_count / COLUMN_COUNT.to_f).ceil
 end
 
 def ljust_dir_items(dir_items)
@@ -19,14 +19,14 @@ def remove_hidden_dir_items(dir_items)
   dir_items.delete_if { |dir_item| dir_item.match?(/^\./) }
 end
 
-def generate_rows(dir_items, num_of_row)
+def generate_rows(dir_items, row_count)
   rows = []
-  num_of_row.times { rows.push([]) }
+  row_count.times { rows.push([]) }
 
   row_index = 0
   dir_items.each do |dir_item|
     rows[row_index].push(dir_item)
-    row_index == num_of_row - 1 ? row_index = 0 : row_index += 1
+    row_index == row_count - 1 ? row_index = 0 : row_index += 1
   end
 
   rows
@@ -37,7 +37,7 @@ ljusted_dir_items = ljust_dir_items(dir_items)
 
 rows = generate_rows(
   remove_hidden_dir_items(ljusted_dir_items),
-  calc_num_of_row(ljusted_dir_items.size)
+  calc_row_count(ljusted_dir_items.size)
 )
 
 rows.each { |row| puts row.join }
