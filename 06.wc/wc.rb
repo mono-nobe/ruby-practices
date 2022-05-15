1ad16ee80ad66623537793bc4a20bd4380688181
mononobe <ryoya.goemono@gmail.com>
2022-05-15 23:22:49 +0900

    標準入力を受け取れるよう実装

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,16 +6,19 @@ require 'optparse'
 SINGLE_FILE_COUNT = 2
 
 def main
-  is_files = !ARGV.empty?
+  is_line = false
+  opt = OptionParser.new
+  opt.on('-l') { is_line = true }
+  opt.parse!(ARGV)
 
-  if is_files
-    show_file_details
+  if !ARGV.empty?
+    show_file_details(is_line)
   else
-    puts 'hoge'
+    show_stdin_details(is_line)
   end
 end
 
-def show_file_details
+def show_file_details(is_line)
   file_details = calc_file_details
   return if file_details.size.zero?
 
@@ -35,16 +38,18 @@ def show_file_details
     file_name: calc_max_value_length(file_details, :file_name)
   }
 
-  print_file_details(file_details, max_detail_length)
+  print_file_details(file_details, max_detail_length, is_line)
 end
 
-def print_file_details(file_details, max_detail_length)
+def print_file_details(file_details, max_detail_length, is_line)
   details = file_details.size == SINGLE_FILE_COUNT ? file_details.slice(0..0) : file_details
 
   details.map do |detail|
-    print " #{detail[:line_count].to_s.rjust(max_detail_length[:line_count])}"
-    print "  #{detail[:word_count].to_s.rjust(max_detail_length[:word_count])}"
-    print " #{detail[:size].to_s.rjust(max_detail_length[:size])}"
+    print detail[:line_count].to_s.rjust(max_detail_length[:line_count])
+    unless is_line
+      print " #{detail[:word_count].to_s.rjust(max_detail_length[:word_count])}"
+      print " #{detail[:size].to_s.rjust(max_detail_length[:size])}"
+    end
     puts " #{detail[:file_name].to_s.ljust(max_detail_length[:file_name])}"
   end
 end
@@ -83,6 +88,30 @@ def calc_file_details
       file_name: file_name
     }
   end.compact
+end
+
+def show_stdin_details(is_line)
+  strings = $stdin.readlines
+
+  word_count = strings.map do |string|
+    string.split(' ').size
+  end.sum
+
+  details = {
+    row: strings.size,
+    word_count: word_count,
+    byte: strings.join.bytesize
+  }
+
+  print_stdin_details(details, is_line)
+end
+
+def print_stdin_details(detail, is_line)
+  if is_line
+    puts detail[:row]
+  else
+    puts "#{detail[:row]} #{detail[:word_count]} #{detail[:byte]}"
+  end
 end
 
 main
