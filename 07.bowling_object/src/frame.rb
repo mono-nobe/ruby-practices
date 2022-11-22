f2193b340857d7bd7c03e6e54d518132170e1fad
mono-nobe <ryoya.goemono@gmail.com>
2022-11-22 22:52:42 +0900

    frameクラスの新規実装

---
+++ 07.bowling_object/src/frame.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative './shot'
+
+# Frameクラスは各フレームのスコアを保持するオブジェクト
+class Frame
+  attr_reader :first_shot,
+              :second_shot,
+              :third_shot
+
+  def initialize(first_mark, second_mark, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
+  end
+
+  def score
+    first_shot.score + second_shot.score + third_shot.score
+  end
+
+  def spare?
+    first_shot.score + second_shot.score == 10
+  end
+
+  def strike?
+    first_shot.score == 10
+  end
+end
---
+++ 07.bowling_object/test/test_frame.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../src/frame'
+
+# Frameクラスのテスト
+class TestCaseFrame < Test::Unit::TestCase
+  # scoreのテスト
+  test '1回目3本、2回目5本倒した時のスコア' do
+    frame = Frame.new(3, 5)
+    assert_equal 8, frame.score
+  end
+
+  # spare?のテスト
+  test 'スペアである' do
+    frame = Frame.new(4, 6)
+    assert frame.spare?
+  end
+
+  test 'スペアではない' do
+    frame = Frame.new(1, 2)
+    refute frame.spare?
+  end
+
+  # strike?のテスト
+  test 'ストライクである' do
+    frame = Frame.new(10, 0)
+    assert frame.strike?
+  end
+
+  test 'ストライクではない' do
+    frame = Frame.new(3, 3)
+    refute frame.strike?
+  end
+end
