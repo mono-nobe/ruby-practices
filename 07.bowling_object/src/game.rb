8f8a812ce1dadb099f703bf6ef66fc1112a25fb3
mono-nobe <ryoya.goemono@gmail.com>
2022-11-29 21:25:00 +0900

    Gameクラスにおける、不要なスコア計算メソッドの削除 外部クラスから参照されないメソッドをprivateメソッドとして再定義

--- 07.bowling_object/src/game.rb
+++ 07.bowling_object/src/game.rb
@@ -9,66 +9,19 @@ SHOT_COUNTS_BY_FRAME = 2
 class Game
   def initialize(marks)
     @marks = marks.shift.split(',')
-    @frames = devide_marks_by_frame.map do |devided_mark_set|
-      Frame.new(devided_mark_set[0], devided_mark_set[1], devided_mark_set[2])
+    @frames = devide_marks_by_frame.map do |set_of_marks_by_frame|
+      Frame.new(set_of_marks_by_frame)
     end
   end
 
-  def score
+  def calc_score
     score = 0
 
     @frames.each_with_index do |frame, index|
-      score += frame.score
-      score += bonus_score(@frames[index..]) unless @frames[index..].size == 1
+      score += frame.calc_frame_total_score(@frames[index..])
     end
 
     score
-  end
-
-  def bonus_score(frames)
-    if frames.first.strike?
-      strike_bonus_score(frames)
-    elsif frames.first.spare?
-      frames[1].first_shot.score
-    else
-      0
-    end
-  end
-
-  def strike_bonus_score(frames)
-    if frames.size == 2
-      last_strike_bonus_score(frames)
-    elsif frames[1].strike?
-      frames[1].score + frames[2].first_shot.score
-    else
-      frames[1].score
-    end
-  end
-
-  def last_strike_bonus_score(frames)
-    frames.last.first_shot.score + frames.last.second_shot.score
-  end
-
-  def next_frame?(frame)
-    return true if first_frame?(frame)
-
-    !last_frame?(frame) && (full_shot?(frame) || strike?(frame))
-  end
-
-  def first_frame?(frame)
-    frame.size.zero?
-  end
-
-  def strike?(frame)
-    frame.last.first == 'X'
-  end
-
-  def full_shot?(frame)
-    frame.last.size == SHOT_COUNTS_BY_FRAME
-  end
-
-  def last_frame?(frame)
-    frame.size == TOTAL_FRAME_COUNTS
   end
 
   private
@@ -81,5 +34,27 @@ class Game
     end
 
     sets_of_marks
+  end
+
+  def next_frame?(sets_of_marks)
+    return true if first_frame?(sets_of_marks)
+
+    !last_frame?(sets_of_marks) && (full_shot?(sets_of_marks.last) || strike?(sets_of_marks.last))
+  end
+
+  def first_frame?(sets_of_marks)
+    sets_of_marks.size.zero?
+  end
+
+  def last_frame?(sets_of_marks)
+    sets_of_marks.size == TOTAL_FRAME_COUNTS
+  end
+
+  def full_shot?(sets_of_marks)
+    sets_of_marks.size == SHOT_COUNTS_BY_FRAME
+  end
+
+  def strike?(set_of_marks)
+    set_of_marks.first == 'X'
   end
 end
--- 07.bowling_object/test/test_game.rb
+++ 07.bowling_object/test/test_game.rb
@@ -13,31 +13,31 @@ class TestCaseGame < Test::Unit::TestCase
 
   test '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5の場合' do
     game = Game.new(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'])
-    assert_equal 139, game.score
+    assert_equal 139, game.calc_score
   end
 
   test '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,Xの場合' do
     game = Game.new(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'])
-    assert_equal 164, game.score
+    assert_equal 164, game.calc_score
   end
 
   test '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4の場合' do
     game = Game.new(['0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'])
-    assert_equal 107, game.score
+    assert_equal 107, game.calc_score
   end
 
   test '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0の場合' do
     game = Game.new(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'])
-    assert_equal 134, game.score
+    assert_equal 134, game.calc_score
   end
 
   test '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8の場合' do
     game = Game.new(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'])
-    assert_equal 144, game.score
+    assert_equal 144, game.calc_score
   end
 
   test 'X,X,X,X,X,X,X,X,X,X,X,Xの場合' do
     game = Game.new(['X,X,X,X,X,X,X,X,X,X,X,X'])
-    assert_equal 300, game.score
+    assert_equal 300, game.calc_score
   end
 end
