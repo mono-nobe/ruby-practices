616120ea1c034b46441fac7950512ac46c7d0625
mono-nobe <ryoya.goemono@gmail.com>
2022-12-02 00:35:57 +0900

    shotクラスに全ピン倒したかを判定するメソッドを追加

--- 07.bowling_object/src/shot.rb
+++ 07.bowling_object/src/shot.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+ALL_PINS_MARK = 'X'
+
 class Shot
   def initialize(mark)
     @mark = mark
   end
 
+  def all_pins?
+    @mark == ALL_PINS_MARK
+  end
+
   def score
-    return 10 if @mark == 'X'
+    return 10 if @mark == ALL_PINS_MARK
 
     @mark.to_i
   end
