0edd5596f266f6a66cb76995ada46fa646a71184
mono-nobe <ryoya.goemono@gmail.com>
2022-12-23 01:08:12 +0900

    メソッド名および最大文字数取得処理を修正

--- 08.ls_object/src/command.rb
+++ 08.ls_object/src/command.rb
@@ -21,9 +21,9 @@ class Command
     if @option_l
       total_blocks = ls_files.sum(&:blocks)
       puts "total #{total_blocks}"
-      puts format_details(ls_files)
+      puts box_details(ls_files)
     else
-      format_names(ls_files).each do |name_row|
+      box_names(ls_files).each do |name_row|
         puts name_row.join
       end
     end
@@ -32,53 +32,73 @@ class Command
   private
 
   def extract_ls_files
-    Dir.glob('*', File::FNM_DOTMATCH).map do |name|
+    Dir.glob('*', File::FNM_DOTMATCH).filter_map do |name|
       next if !@option_a && name.start_with?('.')
 
       LsFile.new(name)
-    end.compact
+    end
   end
 
-  def format_detail(ls_file, hard_link_length, user_name_length, group_name_length, size_length)
+  def box_details(ls_files)
+    hard_links = get_prop_strings(ls_files, 'hard_link')
+    user_names = get_prop_strings(ls_files, 'user_name')
+    group_names = get_prop_strings(ls_files, 'group_name')
+    sizes = get_prop_strings(ls_files, 'size')
+
+    prop_max_lengths = {
+      hard_link: calc_prop_max_length(hard_links),
+      user_name: calc_prop_max_length(user_names),
+      group_name: calc_prop_max_length(group_names),
+      size: calc_prop_max_length(sizes)
+    }
+
+    ls_files.map do |ls_file|
+      format_detail(ls_file, prop_max_lengths)
+    end
+  end
+
+  def get_prop_strings(ls_files, prop_name)
+    ls_files.map do |ls_file|
+      ls_file.send(prop_name).to_s
+    end
+  end
+
+  def format_detail(ls_file, prop_max_lengths)
     symbolic_mode = ls_file.symbolic_mode
-    hard_link = ls_file.hard_link.to_s.rjust(hard_link_length)
-    user_name = ls_file.user_name.ljust(user_name_length)
-    group_name = ls_file.group_name.ljust(group_name_length)
-    size = ls_file.size.to_s.rjust(size_length)
+    hard_link = ls_file.hard_link.to_s.rjust(prop_max_lengths[:hard_link])
+    user_name = ls_file.user_name.ljust(prop_max_lengths[:user_name])
+    group_name = ls_file.group_name.ljust(prop_max_lengths[:group_name])
+    size = ls_file.size.to_s.rjust(prop_max_lengths[:size])
     updated_time = ls_file.updated_time
     name = ls_file.name
 
     "#{symbolic_mode}  #{hard_link} #{user_name}  #{group_name}  #{size} #{updated_time} #{name}"
   end
 
-  def format_details(ls_files)
-    ls_files.each_with_object([]) do |ls_file, details|
-      details << format_detail(
-        ls_file,
-        calc_prop_max_length(ls_files, 'hard_link'),
-        calc_prop_max_length(ls_files, 'user_name'),
-        calc_prop_max_length(ls_files, 'group_name'),
-        calc_prop_max_length(ls_files, 'size')
-      )
-    end
-  end
-
-  def format_names(ls_files)
-    name_max_length = calc_prop_max_length(ls_files, 'name')
-    names = ls_files.map do |ls_file|
-      ls_file.name.ljust(name_max_length + 1)
-    end
+  def box_names(ls_files)
+    names = ls_files.map(&:name)
+    formated_names = format_names(names)
 
     row_count = (names.size / COLUMN_COUNT.to_f).ceil
-    names.each_slice(row_count).map do |name_row|
-      name_row << nil while name_row.size < row_count
+    formated_name_rows = formated_names.each_slice(row_count)
+    formated_name_rows.map do |name_row|
+      name_row << nil while row_full?(name_row, row_count)
       name_row
     end.transpose
   end
 
-  def calc_prop_max_length(ls_files, prop_name)
-    ls_files.map do |ls_file|
-      ls_file.send(prop_name).to_s
-    end.max_by(&:length).length
+  def format_names(names)
+    name_max_length = calc_prop_max_length(names)
+    names.map do |name|
+      name.ljust(name_max_length + 1)
+    end
+  end
+
+  def row_full?(row, row_count)
+    row.size < row_count
+  end
+
+  def calc_prop_max_length(strings)
+    strings.max_by(&:length).length
   end
 end
